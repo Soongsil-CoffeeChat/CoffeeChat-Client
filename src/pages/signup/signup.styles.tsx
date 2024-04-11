07f34b01109d07df3715f4ff9a17bd813954c872
choiseohyeon <csh200310@naver.com>
2024-04-11 21:11:45 +0900

    [desig] 전체적인 뷰 완료

--- src/pages/signup/signup.styles.tsx
+++ src/pages/signup/signup.styles.tsx
@@ -66,4 +66,84 @@ export const EmailReceiveBtn = styled.button`
   border: 1px solid #02a6cb;
   border-radius: 7px;
   cursor: pointer;
+  font-size: 15px;
+`;
+
+export const NicknameInputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  border: 1px solid #02a6cb;
+  border-radius: 7px;
+  margin-top: 30px;
+`;
+
+export const NicknameInput = styled.input`
+  width: 60%;
+  height: 50%;
+  font-size: 20px;
+  border: none;
+  padding: 12px 15px;
+  margin-left: 5px;
+`;
+
+export const NicknameBtn = styled.button`
+  width: 20%;
+  height: 40px;
+  padding: 12px 15px;
+  margin-top: 5px;
+  margin-left: 45px;
+  margin-right: 5px;
+  background-color: #e2f9ff;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+`;
+
+export const FieldContainer = styled.div`
+  margin-top: 30px;
+  flex-direction: column;
+`;
+export const FieldText = styled.div`
+  font-size: 20px;
+  color: #000000;
+`;
+
+export const FieldButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+interface FieldButtonProps {
+  active: boolean;
+}
+
+export const FieldButton = styled.div<FieldButtonProps>`
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 15px;
+  background-color: ${(props) => (props.active ? "#65DDFA" : "#e9fbff")};
+  border-radius: 13px;
+  padding: 12px 25px;
+  &:hover {
+    opacity: 0.8;
+  }
+  margin-top: 20px;
+`;
+
+export const StartBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  background: linear-gradient(180deg, #62c6c4 0%, #02a6cb 100%);
+  border: none;
+  border-radius: 7px;
+  margin-top: 40px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
 `;
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import LeftArrow from "../../assets/ArrowLeft.svg";
 import * as styles from "./signup.styles";
 
@@ -8,7 +8,9 @@ import * as styles from "./signup.styles";
 function SignUp() {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>("");
-  const [domain, setDomain] = useState<string>("@gmail.com");
+  const [domain, setDomain] = useState<string>("직접입력");
+  const [nickname, setNickname] = useState<string>("");
+  const [activeButton, setActiveButton] = useState<string>("");
 
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -18,8 +20,20 @@ function SignUp() {
     setDomain(event.target.value);
   };
 
+  const handleNicknameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNickname(event.target.value);
+  };
+
   const goToLogin = () => {
     navigate("/login");
+  };
+
+  useEffect(() => {
+    // 클릭한 분야 변경 시 데이터 가져오기
+  }, [activeButton]);
+
+  const handleButtonClick = (buttonName: string) => {
+    setActiveButton((prev) => (prev === buttonName ? "" : buttonName));
   };
 
   return (
@@ -31,7 +45,11 @@ function SignUp() {
             alt="arrowBnt"
             onClick={goToLogin}
           />
-          <styles.HeaderTitle>이메일로 가입해 주세요 </styles.HeaderTitle>
+          <styles.HeaderTitle>
+            코고에 가입하려면
+            <br />
+            추가정보가 필요해요{" "}
+          </styles.HeaderTitle>
           <styles.HeaderText>
             개인정보는 정보통신망법에 따라 안전하게 보관됩니다.
           </styles.HeaderText>
@@ -53,6 +71,7 @@ function SignUp() {
             />
 
             <styles.EmailSelect value={domain} onChange={handleDomainChange}>
+              <option value="@self">직접입력</option>
               <option value="@gmail.com">@gmail.com</option>
               <option value="@naver.com">@naver.com</option>
               <option value="@soongsil.ac.kr">@soongsil.ac.kr</option>
@@ -61,6 +80,35 @@ function SignUp() {
             </styles.EmailSelect>
           </styles.EmailContainer>
           <styles.EmailReceiveBtn>이메일 받기</styles.EmailReceiveBtn>
+          <styles.NicknameInputContainer>
+            <styles.NicknameInput
+              type="nickname"
+              name="nickname"
+              placeholder="닉네임을 입력해주세요."
+              value={nickname}
+              onChange={handleNicknameChange}
+            />
+            <styles.NicknameBtn>확인</styles.NicknameBtn>
+          </styles.NicknameInputContainer>
+          <styles.FieldContainer>
+            <styles.FieldText>
+              나의 관심사 또는 희망하는
+              <br />
+              분야을 선택해주세요
+            </styles.FieldText>
+            <styles.FieldButtonContainer>
+              {["기획", "FE", "BE", "디자인"].map((buttonName) => (
+                <styles.FieldButton
+                  key={buttonName}
+                  active={activeButton.includes(buttonName)}
+                  onClick={() => handleButtonClick(buttonName)}
+                >
+                  {buttonName}
+                </styles.FieldButton>
+              ))}
+            </styles.FieldButtonContainer>
+          </styles.FieldContainer>
+          <styles.StartBtn>시작하기</styles.StartBtn>
         </styles.InputContainer>
       </styles.Container>
     </>
