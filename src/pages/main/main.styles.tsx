204a77a210fea504b89d1cc5710c124104f2d820
ChoiSangwon <fdsa200@naver.com>
2024-04-17 16:18:46 +0900

    refactor : 주석 제거

--- src/pages/main/main.styles.tsx
+++ src/pages/main/main.styles.tsx
@@ -10,9 +10,9 @@ export const HeaderContainer = styled.div`
 `;
 
 export const HeaderTitle = styled.div`
-display: flex;
-justify-content: space-between; // 양쪽으로 내용을 분산시킵니다.
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   font-weight: bold;
   font-size: 15px;
 `;
@@ -34,8 +34,7 @@ export const HeaderButton = styled.div<HeaderButtonProps>`
   cursor: pointer;
   font-weight: bold;
   font-size: 15px;
-  background-color: ${(props) =>
-    props.active ? "#02A6CB" : "#e9fbff"}; // 활성 상태에 따라 배경색 변경
+  background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
   border-radius: 13px;
   padding: 12px 18px;
   &:hover {
@@ -44,11 +43,9 @@ export const HeaderButton = styled.div<HeaderButtonProps>`
 `;
 
 export const HeaderProfileButton = styled.div<HeaderButtonProps>`
-  padding: 15px 15px; // 예시 패딩, 필요에 따라 조정하세요
-  border-radius: 20px; // 동그란 모양을 만들기 위해 border-radius 값을 적당히 조정
-  border: 1px solid #000; // 예시 테두리, 필요에 따라 조정하세요
-  background-color: ${(props) =>
-    props.active ? "#02A6CB" : "#e9fbff"}; // 활성 상태에 따라 배경색 변경
-  color: #000; // 텍스트 색상
-
+  padding: 15px 15px;
+  border-radius: 20px;
+  border: 1px solid #000;
+  background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
+  color: #000;
 `;
