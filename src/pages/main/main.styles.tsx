ad70a744757c5d6a61c6d8224f3c266b42b67132
ChoiSangwon <fdsa200@naver.com>
2024-04-17 18:42:56 +0900

    design : 메인화면 디자인 추가

--- src/pages/main/main.styles.tsx
+++ src/pages/main/main.styles.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
 
 export const HeaderContainer = styled.div`
   display: block;
   margin: 10px;
   width: 100%;
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
 `;
 
 export const HeaderTitle = styled.div`
@@ -37,6 +39,7 @@ export const HeaderButton = styled.div<HeaderButtonProps>`
   background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
   border-radius: 13px;
   padding: 12px 18px;
+  margin: 15px 10px;
   &:hover {
     opacity: 0.8;
   }
@@ -45,7 +48,101 @@ export const HeaderButton = styled.div<HeaderButtonProps>`
 export const HeaderProfileButton = styled.div<HeaderButtonProps>`
   padding: 15px 15px;
   border-radius: 20px;
-  border: 1px solid #000;
+  border: 1px solid black;
   background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
   color: #000;
+`;
+
+export const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 80%;
+`;
+
+export const BodyProfile = styled.div`
+  flex: 4.5;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: linear-gradient(
+    to left,
+    rgba(137, 105, 172, 0.75) 0%,
+    rgba(253, 214, 85, 0.75) 100%
+  );
+  position: relative;
+`;
+export const ImageButton = styled.img`
+  margin: 1rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+export const ProfileCircle = styled.div`
+  width: 196px;
+  height: 196px;
+  background-color: white;
+  border-radius: 50%;
+  flex: none;
+`;
+export const ProfileTopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  width: 100%;
+`;
+
+export const ProfileIcon = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+  background-color: white;
+  border-radius: 13px;
+  padding: 0.3rem 1rem;
+  margin: 0 0.5rem;
+`;
+
+export const ProfileBottomContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+`;
+
+export const BodyIntroduce = styled.div`
+  flex: 5;
+  display: flex;
+  justify-content: space-around;
+  height: 50%;
+  flex-direction: column;
+  margin-left: 1rem;
+  margin-right: 1rem;
+`;
+
+export const BodyIntroduceHeader = styled.div`
+  margin-top: 10px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const BodyIntroduceText = styled.div`
+  margin: 10px 0;
+  max-height: 70%;
+  overflow-y: auto;
+  font-size: 12px;
+`;
+
+export const ApplyButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to left, #62c6c4 0%, #02a6cb 100%);
+  width: 360px;
+  height: 50px;
+  border-radius: 7px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
 `;
