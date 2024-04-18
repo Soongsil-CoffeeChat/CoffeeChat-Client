c142076e5a6518476c14ffa735dc62e911f12953
ChoiSangwon <fdsa200@naver.com>
2024-04-18 22:33:57 +0900

    [Design] 메인 페이지 화면 구현 (#11)
    * refactor : 주석 제거
    
    * feat : add 메인페이지 왼쪽,오른쪽 버튼 svg 추가
    
    * design : 메인화면 디자인 추가
    
    * design : margin 조정
    
    * fix : border radius 변경

--- src/pages/main/main.styles.tsx
+++ src/pages/main/main.styles.tsx
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
 
 export const HeaderContainer = styled.div`
   display: block;
-  margin: 10px;
+  margin-bottom: 1rem;
+  padding-top: 10px;
   width: 100%;
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
 `;
 
 export const HeaderTitle = styled.div`
-display: flex;
-justify-content: space-between; // 양쪽으로 내용을 분산시킵니다.
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   font-weight: bold;
   font-size: 15px;
 `;
@@ -34,21 +37,114 @@ export const HeaderButton = styled.div<HeaderButtonProps>`
   cursor: pointer;
   font-weight: bold;
   font-size: 15px;
-  background-color: ${(props) =>
-    props.active ? "#02A6CB" : "#e9fbff"}; // 활성 상태에 따라 배경색 변경
+  background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
   border-radius: 13px;
   padding: 12px 18px;
+  margin: 15px 10px;
   &:hover {
     opacity: 0.8;
   }
 `;
 
 export const HeaderProfileButton = styled.div<HeaderButtonProps>`
-  padding: 15px 15px; // 예시 패딩, 필요에 따라 조정하세요
-  border-radius: 20px; // 동그란 모양을 만들기 위해 border-radius 값을 적당히 조정
-  border: 1px solid #000; // 예시 테두리, 필요에 따라 조정하세요
-  background-color: ${(props) =>
-    props.active ? "#02A6CB" : "#e9fbff"}; // 활성 상태에 따라 배경색 변경
-  color: #000; // 텍스트 색상
+  padding: 15px 15px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: ${(props) => (props.active ? "#02A6CB" : "#e9fbff")};
+  color: #000;
+`;
 
+export const BodyContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  height: 80%;
+`;
+
+export const BodyProfile = styled.div`
+  flex: 4.5;
+  border-radius: 13px 13px 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: linear-gradient(
+    to left,
+    rgba(137, 105, 172, 0.75) 0%,
+    rgba(253, 214, 85, 0.75) 100%
+  );
+  position: relative;
+`;
+export const ImageButton = styled.img`
+  margin: 1rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+export const ProfileCircle = styled.div`
+  width: 196px;
+  height: 196px;
+  background-color: white;
+  border-radius: 50%;
+  flex: none;
+`;
+export const ProfileTopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  width: 100%;
+`;
+
+export const ProfileIcon = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+  background-color: white;
+  border-radius: 13px;
+  padding: 0.3rem 1rem;
+  margin: 0 0.5rem;
+`;
+
+export const ProfileBottomContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+`;
+
+export const BodyIntroduce = styled.div`
+  flex: 5;
+  display: flex;
+  justify-content: space-around;
+  height: 50%;
+  flex-direction: column;
+  margin-left: 1rem;
+  margin-right: 1rem;
+`;
+
+export const BodyIntroduceHeader = styled.div`
+  margin-top: 10px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const BodyIntroduceText = styled.div`
+  margin: 10px 0;
+  max-height: 70%;
+  overflow-y: auto;
+  font-size: 12px;
+`;
+
+export const ApplyButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to left, #62c6c4 0%, #02a6cb 100%);
+  width: 360px;
+  height: 50px;
+  border-radius: 7px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
 `;
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import * as styles from "./main.styles";
 import { useNavigate } from "react-router-dom";
+import LeftButton from "../../assets/LeftButton.svg";
+import RightButton from "../../assets/RightButton.svg";
 
 function Main() {
   const [activeButtons, setActiveButtons] = useState<string[]>([]);
@@ -31,15 +33,15 @@ function Main() {
         <styles.HeaderTitle>
           어떤 동아리 선배가 있을까요?
           <styles.HeaderButtonContainer>
-            {[""].map((buttonName)=> (
+            {[""].map((buttonName) => (
               <styles.HeaderProfileButton
-              key={buttonName}
-              active={activeButtons.includes(buttonName)}
-              onClick={() => handleProfileButtonClick(buttonName)}>
-              {buttonName}
+                key={buttonName}
+                active={activeButtons.includes(buttonName)}
+                onClick={() => handleProfileButtonClick(buttonName)}>
+                {buttonName}
               </styles.HeaderProfileButton>
             ))}
-            </styles.HeaderButtonContainer>
+          </styles.HeaderButtonContainer>
         </styles.HeaderTitle>
         <styles.HeaderText>동아리별 코고 선배 알아보기</styles.HeaderText>
         <styles.HeaderButtonContainer>
@@ -47,13 +49,62 @@ function Main() {
             <styles.HeaderButton
               key={buttonName}
               active={activeButtons.includes(buttonName)}
-              onClick={() => handleButtonClick(buttonName)}
-            >
+              onClick={() => handleButtonClick(buttonName)}>
               {buttonName}
             </styles.HeaderButton>
           ))}
         </styles.HeaderButtonContainer>
       </styles.HeaderContainer>
+      <styles.BodyContainer>
+        <styles.BodyProfile>
+          <styles.ProfileTopContainer>
+            <styles.ImageButton src={LeftButton} alt="Left" />
+            <styles.ProfileCircle />
+            <styles.ImageButton src={RightButton} alt="Right" />
+          </styles.ProfileTopContainer>
+          <styles.ProfileBottomContainer>
+            <styles.ProfileIcon>ㅎㅇ</styles.ProfileIcon>
+            <styles.ProfileIcon>ㅎㅇ</styles.ProfileIcon>
+            <styles.ProfileIcon>ㅎㅇ</styles.ProfileIcon>
+          </styles.ProfileBottomContainer>
+        </styles.BodyProfile>
+        <styles.BodyIntroduce>
+          <styles.BodyIntroduceHeader>
+            {"아픈건 딱 질색이니까"}
+          </styles.BodyIntroduceHeader>
+          <styles.BodyIntroduceText>
+            {
+              "오늘도 아침엔 입에 빵을 물고야 평온했던 하늘이 무너지고어둡던 눈앞이\
+            붉어지며뭔가 잊고 온 게 있는 것 같아괜히 이상하게 막 울 것만\
+            같고그냥 지나치는 게 나을 것 같아나는 생각은 딱 질색이니까 오랫동안\
+            나를 아는슬픈 표정을 하고 oh, oh흔적 없는 기억 밖혹 과거에 미래에 딴\
+            차원에 세계에One, two, three, four, five, six, seven, eight 평온했던\
+            하늘이 무너지고어둡던 눈앞이 붉어져도다시 놓쳐버리는 것만 같아괜히\
+            이상하게 막 울 것만 같고 오늘도 아침엔 입에 빵을 물고야 평온했던\
+            하늘이 무너지고어둡던 눈앞이 붉어지며뭔가 잊고 온 게 있는 것\
+            같아괜히 이상하게 막 울 것만 같고그냥 지나치는 게 나을 것 같아나는\
+            생각은 딱 질색이니까 오랫동안 나를 아는슬픈 표정을 하고 oh, oh흔적\
+            없는 기억 밖혹 과거에 미래에 딴 차원에 세계에One, two, three, four,\
+            five, six, seven, eight 평온했던 하늘이 무너지고어둡던 눈앞이\
+            붉어져도다시 놓쳐버리는 것만 같아괜히 이상하게 막 울 것만 같고\
+            오늘도 아침엔 입에 빵을 물고야 평온했던 하늘이 무너지고어둡던 눈앞이\
+            붉어지며뭔가 잊고 온 게 있는 것 같아괜히 이상하게 막 울 것만\
+            같고그냥 지나치는 게 나을 것 같아나는 생각은 딱 질색이니까 오랫동안\
+            나를 아는슬픈 표정을 하고 oh, oh흔적 없는 기억 밖혹 과거에 미래에 딴\
+            차원에 세계에One, two, three, four, five, six, seven, eight 평온했던\
+            하늘이 무너지고어둡던 눈앞이 붉어져도다시 놓쳐버리는 것만 같아괜히\
+            이상하게 막 울 것만 같고 오늘도 아침엔 입에 빵을 물고야 평온했던\
+            하늘이 무너지고어둡던 눈앞이 붉어지며뭔가 잊고 온 게 있는 것\
+            같아괜히 이상하게 막 울 것만 같고그냥 지나치는 게 나을 것 같아나는\
+            생각은 딱 질색이니까 오랫동안 나를 아는슬픈 표정을 하고 oh, oh흔적\
+            없는 기억 밖혹 과거에 미래에 딴 차원에 세계에One, two, three, four,\
+            five, six, seven, eight 평온했던 하늘이 무너지고어둡던 눈앞이\
+            붉어져도다시 놓쳐버리는 것만 같아괜히 이상하게 막 울 것만 같고"
+            }
+          </styles.BodyIntroduceText>
+          <styles.ApplyButton>코고 신청하기</styles.ApplyButton>
+        </styles.BodyIntroduce>
+      </styles.BodyContainer>
     </styles.Container>
   );
 }
