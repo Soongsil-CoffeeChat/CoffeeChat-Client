d0fcc035e80ba01c3ce38aa40d5e219019c470ab
choiseohyeon <csh200310@naver.com>
2024-05-28 14:15:20 +0900

    [feat] 로그인 로직 변경

--- src/atoms/authState.ts
+++ src/atoms/authState.ts
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
 
-export const authState = atom({
+export interface AuthState {
+  isLoggedIn: boolean;
+  username: string | null;
+  token: string | null;
+}
+
+export const authState = atom<AuthState>({
   key: "authState",
   default: {
     isLoggedIn: false,
